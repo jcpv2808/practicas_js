0a4b8524122beac2c3640678b9dae481c31c527c
Jean Pinto <jcp280899@gmail.com>
2022-10-12 23:46:30 -0500

    agregado pc56

--- pc56/js/dom.js
+++ pc56/js/dom.js
@@ -8,4 +8,24 @@ console.log(document.documentElement.lang)
 document.documentElement.setAttribute("lang","es-MX")
 console.log(document.documentElement.lang)
 
-const linkDom = document.querySelector(".link-dom")
+const $linkDom = document.querySelector(".link-dom")
+
+$linkDom.setAttribute("target","_blank")
+$linkDom.setAttribute("rel","noopener")
+$linkDom.setAttribute("href","https://www.youtube.com")
+
+console.log($linkDom.hasAttribute("rel"))
+$linkDom.removeAttribute("rel")
+console.log($linkDom.hasAttribute("rel"))
+
+//data -attributes
+console.log($linkDom.getAttribute("data-description"))
+console.log($linkDom.dataset)
+console.log($linkDom.dataset.description)
+$linkDom.setAttribute("data-description","Modelo de Objeto del Documento")
+console.log($linkDom.dataset.description)
+$linkDom.dataset.description = "Link para ir a YouTube"
+console.log($linkDom.dataset.description)
+console.log($linkDom.hasAttribute("data-id"))
+$linkDom.removeAttribute("data-id")
+console.log($linkDom.hasAttribute("data-id"))
