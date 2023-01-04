41a93b75253191ce03de7ab79477521f134bafa0
Jean Pinto <jcp280899@gmail.com>
2023-01-04 13:44:50 -0500

    pc60 completa

---
+++ pc60/js/dom.js
@@ -0,0 +1,22 @@
+const $cards = document.querySelector('.cards')
+console.log($cards)
+console.log($cards.children)
+console.log($cards.children[2])
+console.log($cards.parentElement)
+console.log($cards.firstElementChild)
+console.log($cards.lastElementChild)
+console.log($cards.previousElementSibling)
+console.log($cards.nextElementSibling)
+console.log($cards.closest("body"))
+console.log($cards.children[2].closest("section"))
+
+
+
+
+
+//$cards.lastElementChild.classList.add("rotate-45")
+
+
+
+
+
