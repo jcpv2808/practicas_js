ed5a0b5d739f5007a0c43cda533e764fb877ef87
Jean Pinto <jcp280899@gmail.com>
2023-01-05 15:09:23 -0500

    pc65 terminado

---
+++ pc65/js/dom.js
@@ -0,0 +1,23 @@
+function holaMundo() {
+    alert("Hola Mundo")
+    console.log(event)
+}
+
+const $event_semantico = document.getElementById("btn_semantico"),
+    $event_multiple = document.getElementById("btn_multiple");
+
+//$event_semantico.onclick = holaMundo;
+$event_semantico.onclick = (e) => {
+    alert("Hola Mundo Semantico");
+    console.log(e);
+};
+
+//$event_multiple.addEventListener("click", holaMundo);
+$event_multiple.addEventListener("click", (e) =>{
+        alert("Hola mundo Multiple")
+        console.log(e)
+        console.log(e.type)
+        console.log(e.target)
+        console.log(event)
+    });
+
