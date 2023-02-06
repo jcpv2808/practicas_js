32d300f0a35ff1912adedba99ff4c65b35c2c36a
Jean Pinto <jcp280899@gmail.com>
2023-02-06 16:23:35 -0500

    deleted pc 77

--- pc 77/js/img.js
+++
@@ -1,6 +0,0 @@
-const $img = document.querySelectorAll(".cards img");
-console.log($img)
-$img.forEach(img => {
-    let src = Math.floor(Math.random() * 100) + 1
-    img.setAttribute("src", `https://picsum.photos/id/${src}/200`)
-})
